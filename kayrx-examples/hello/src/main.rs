2ea444e5c4f04aad9d955b457e095855f2d4cd73
krircc <krircc@aliyun.com>
2020-01-20 18:29:24 +0800

    pass more kaytx examples

--- kayrx-examples/hello/src/main.rs
+++ kayrx-examples/hello/src/main.rs
@@ -1,17 +1,31 @@
 #[macro_use]
 extern crate kayrx;
 
-use kayrx::web::{web, App, HttpServer, Responder};
+use kayrx::web::{middleware, web, App, HttpServer, HttpRequest, Responder};
+
+async fn index(req: HttpRequest) -> &'static str {
+    println!("REQ: {:?}", req);
+    "Hello world!"
+}
 
 #[get("/{id}/{name}/index.html")]
-async fn index(info: web::Path<(u32, String)>) -> impl Responder {
+async fn hello(info: web::Path<(u32, String)>) -> impl Responder {
       format!("Hello {}! id:{}", info.1, info.0)
 }
 
 #[kayrx::main]
 async fn main() -> std::io::Result<()> {
-      HttpServer::new(|| App::new().service(index))
-          .bind("127.0.0.1:8080")?
-          .run()
-          .await
+        std::env::set_var("RUST_LOG", "kayrx::web=info");
+        env_logger::init();
+
+        HttpServer::new(|| 
+            App::new()
+            // enable logger
+            .wrap(middleware::Logger::default())
+            .service(web::resource("/index.html").to(|| async { "Hello world!" }))
+            .service(web::resource("/").to(index))
+            .service(hello))
+            .bind("127.0.0.1:8080")?
+            .run()
+            .await
 }
